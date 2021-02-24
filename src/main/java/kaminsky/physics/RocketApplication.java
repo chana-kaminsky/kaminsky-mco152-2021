3580a4a4f2081c93bf895afb87c2838982d4f646
Chana Kaminsky <chanakaminsky@gmail.com>
2021-02-23 19:21:50 -0500

    made it into a Gradle Project

--- src/main/java/kaminsky/physics/RocketApplication.java
+++ src/main/java/kaminsky/physics/RocketApplication.java
@@ -5,13 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import kaminsky.scrabble.Scrabble;
 
 public class RocketApplication extends Application
 {
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("rocket_application.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/rocket_application.fxml"));
 
         Scene scene = new Scene(root, 400, 500);
 
