cb788174df77848a8e4d26029e19cf6878e28126
Chana Kaminsky <chanakaminsky@gmail.com>
2021-03-16 18:03:50 -0400

    added to scrabble game

--- src/main/java/kaminsky/scrabble/ScrabbleController.java
+++ src/main/java/kaminsky/scrabble/ScrabbleController.java
@@ -6,7 +6,6 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-
 import java.util.List;
 
 public class ScrabbleController
@@ -17,69 +16,98 @@ public class ScrabbleController
     @FXML
     List<Label> bottomLabels;
 
+    @FXML
+    Label points;
+
     public void initialize()
     {
-        setTopLabels();
-        setBottomLabels();
-    }
-
-    private void setTopLabels()
-    {
-        for (int i = 0; i < topLabels.size(); i++)
-        {
-            topLabels.get(i).setMinWidth(50);
-            topLabels.get(i).setMinHeight(50);
-
-            GridPane.setColumnIndex(topLabels.get(i), i);
-            GridPane.setRowIndex(topLabels.get(i), 0);
-
-            BorderStroke stroke = new BorderStroke(null,
-                                  BorderStrokeStyle.SOLID,
-                             null, BorderStroke.THIN);
-            Border border = new Border(stroke);
-            topLabels.get(i).setBorder(border);
+        for (Label label : bottomLabels) {
+            label.setAlignment(Pos.CENTER);
+            label.setText(letterBag.nextLetter());
         }
     }
 
-    private void setBottomLabels()
-    {
-        for (int i = 0; i < bottomLabels.size(); i++)
-        {
-            bottomLabels.get(i).setMinWidth(50);
-            bottomLabels.get(i).setMinHeight(50);
+    private final LetterBag letterBag = new LetterBag();
+    private final Dictionary dictionary = new Dictionary("dictionary.txt");
 
-            GridPane.setColumnIndex(bottomLabels.get(i), i);
-            GridPane.setRowIndex(bottomLabels.get(i), 2);
+//    public ScrabbleController()
+//    {
+//        Dictionary dictionary = new Dictionary("dictionary.txt");
+//    }
 
-            BorderStroke stroke = new BorderStroke(null,
-                                  BorderStrokeStyle.SOLID,
-                             null, BorderStroke.THIN);
-            Border border = new Border(stroke);
-            bottomLabels.get(i).setBorder(border);
-            bottomLabels.get(i).setAlignment(Pos.CENTER);
-            bottomLabels.get(i).setText(letterBag.nextLetter());
-        }
-    }
-
-    LetterBag letterBag = new LetterBag();
-
-    public ScrabbleController()
-    {
-        Dictionary dictionary = new Dictionary("dictionary.txt");
-    }
-
-    public void onAnswerClicked(MouseEvent event)
+    public void onBottomClicked(MouseEvent event)
     {
         Label label = (Label) event.getSource();
+        for (Label topLabel : topLabels)
+        {
+            if (topLabel.getText().isEmpty())
+            {
+                topLabel.setText(label.getText());
+                topLabel.setAlignment(Pos.CENTER);
+                break;
+            }
+        }
+        label.setText("");
     }
 
     public void onClear(ActionEvent event)
     {
-
+        for (Label topLabel : topLabels)
+        {
+            for (Label bottomLabel : bottomLabels)
+            {
+                if (bottomLabel.getText().isEmpty())
+                {
+                    bottomLabel.setText(topLabel.getText());
+                    topLabel.setText("");
+                    break;
+                }
+            }
+        }
     }
 
     public void onSubmit(ActionEvent event)
     {
+        String word = "";
+        int numLetters = 0;
+        for (int i = 0; i < topLabels.size(); i++)
+        {
+            if (!topLabels.get(i).getText().isEmpty())
+            {
+                word += topLabels.get(i).getText();
+            }
+            else
+            {
+                numLetters = i;
+                break;
+            }
+        }
+        if (dictionary.search(word))
+        {
+            addPoints(numLetters);
+            for (int i = 0; i < bottomLabels.size(); i++)
+            {
+                if (bottomLabels.get(i).getText().isEmpty())
+                {
+                    bottomLabels.get(i).setText(letterBag.nextLetter());
+                }
+                topLabels.get(i).setText("");
+            }
+        }
+        else
+        {
+            onClear(event);
+        }
+    }
 
+    public void addPoints(int numLetters)
+    {
+        // assume 6 letters = 9pt and 7 letters = 11pt
+        int numPoints = Integer.parseInt(points.getText());
+        if (numLetters > 1)
+        {
+            numPoints += 2 * numLetters - 3;
+            points.setText(String.valueOf(numPoints));
+        }
     }
 }
